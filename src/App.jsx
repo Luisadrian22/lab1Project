00600c0e2f731276e5a97e4518ce679466504dd7
Luisadrian22 <133919177+Luisadrian22@users.noreply.github.com>
2025-02-25 04:18:02 -0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import Calendar from './Components/Calendar';
+
+const App = () => {
+
+  return (
+    <div className="App">
+      <h1>Karla's Itinerary 🐕</h1>
+      <h2>Welcome Karla! Check out this calendar to keep track of this week's task recommended for you to do </h2>
+      <Calendar/>
+    </div>
+  )
+}
+
+export default App
